55caa32eb2aab3bfa759b48d4e29642003d52191
Michael P <mtpanetta@gmail.com>
2018-06-14 10:45:27 -0500

    finished timer

--- react-client/src/components/Room.jsx
+++ react-client/src/components/Room.jsx
@@ -108,6 +108,8 @@ class Room extends React.Component {
           let time = tock.lap()
           let seconds = (Math.floor((time / 1000) % 60));
           let minutes = (Math.floor((time / (60000)) % 60));
+          seconds = (seconds < 10) ? "0" + seconds : seconds;
+          minutes = (minutes < 10) ? "0" + minutes : minutes;
 
           this.setState({
             timer: minutes + ':' + seconds
